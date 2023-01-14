3cac146703fe68a8bc2764710a480fab2a9e8c7b
Kacper Leśniański <kacper.lesnianski@wp.pl>
2023-01-14 22:32:38 +0100

    Refactor main

--- tsprandom/src/main.rs
+++ tsprandom/src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    sync::{Arc, Mutex},
+    sync::Mutex,
     thread::{self, available_parallelism},
 };
 
@@ -21,51 +21,77 @@ fn main() {
 
     let distance_matrix = file.distance_matrix;
     let local_minimums = Mutex::new(FxHashMap::<Vec<usize>, (i32, i32)>::default());
-    let visited_starting_points = Mutex::new(FxHashSet::default());
+    let visited_starting = Mutex::new(FxHashSet::default());
 
     thread::scope(|s| {
         for _ in 0..thread_count {
             s.spawn(|| {
-                for _ in 0..samples_per_thread {
-                    let mut visited_set = visited_starting_points
-                        .lock()
-                        .expect("Mutex poisoned, bailing out!");
-
-                    let mut starting_solution = random_solution(distance_matrix.len(), None);
-                    let mut retries = 0;
-                    while visited_set.contains(&starting_solution) {
-                        retries += 1;
-                        if retries > MAX_RETRIES {
-                            //can't find any new starting points, end thread
-                            return;
-                        }
-                        starting_solution = random_solution(distance_matrix.len(), None);
-                    }
-
-                    visited_set.insert(starting_solution.clone());
-
-                    //drop the lock to release the mutex
-                    drop(visited_set);
-
-                    let (hillclimb_tour, hillclimb_len) =
-                        hillclimb(&starting_solution, &distance_matrix);
-
-                    let mut map = local_minimums.lock().expect("Mutex poisoned, bailing out!");
-
-                    match map.get_mut(&hillclimb_tour) {
-                        Some(v) => {
-                            v.1 += 1;
-                        }
-                        None => {
-                            map.insert(hillclimb_tour, (hillclimb_len, 1));
-                        }
-                    }
-                }
+                sample(
+                    samples_per_thread,
+                    MAX_RETRIES,
+                    &distance_matrix,
+                    &local_minimums,
+                    &visited_starting,
+                );
             });
         }
     });
 
-    for e in visited_starting_points.lock().unwrap().iter() {
+    for e in visited_starting.into_inner().unwrap() {
         println!("{:?}", e);
     }
+}
+
+fn sample(
+    sample_count: usize,
+    max_retries: usize,
+    distance_matrix: &Vec<Vec<i32>>,
+    local_minimums: &Mutex<FxHashMap<Vec<usize>, (i32, i32)>>,
+    visited_starting: &Mutex<FxHashSet<Vec<usize>>>,
+) {
+    for _ in 0..sample_count {
+        let starting_solution = find_starting_point(visited_starting, distance_matrix, max_retries);
+        if starting_solution.is_none() {
+            return;
+        }
+        let starting_solution = starting_solution.unwrap();
+
+        let (hillclimb_tour, hillclimb_len) = hillclimb(&starting_solution, distance_matrix);
+
+        let mut map = local_minimums.lock().expect("Mutex poisoned, bailing out!");
+
+        match map.get_mut(&hillclimb_tour) {
+            Some(v) => {
+                v.1 += 1;
+            }
+            None => {
+                map.insert(hillclimb_tour, (hillclimb_len, 1));
+            }
+        }
+    }
+}
+
+fn find_starting_point(
+    visited_starting: &Mutex<FxHashSet<Vec<usize>>>,
+    distance_matrix: &Vec<Vec<i32>>,
+    max_retries: usize,
+) -> Option<Vec<usize>> {
+    let mut visited_set = visited_starting
+        .lock()
+        .expect("Mutex poisoned, bailing out!");
+
+    let mut starting_solution = random_solution(distance_matrix.len(), None);
+    let mut retries = 0;
+    while visited_set.contains(&starting_solution) {
+        retries += 1;
+        if retries > max_retries {
+            //can't find any new starting points, end thread
+            return None;
+        }
+        starting_solution = random_solution(distance_matrix.len(), None);
+    }
+
+    visited_set.insert(starting_solution.clone());
+
+    Some(starting_solution)
 }
